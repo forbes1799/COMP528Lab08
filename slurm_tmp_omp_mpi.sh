0ac354aff1e56626baac82389d668b8796d0bc41
forbes1799 <123934016+forbes1799@users.noreply.github.com>
2024-11-27 10:10:11 +0000

    Add files via upload

---
+++ slurm_tmp_omp_mpi.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+
+#SBATCH -p nodes
+#SBATCH -D ./
+#SBATCH -t 15
+#SBATCH --export=ALL
+
+procs=${SLURM_NTASKS:-1} #Gets the requested number of processes when sbatch -n is used
+
+cores=${SLURM_CPUS_PER_TASK:-1} #Gets the requested number of cores when sbatch -c is used
+
+module load compilers/intel/2019u5
+module load mpi/intel-mpi/2019u5/bin
+
+# 1. Add code here to compile a program with mpiicc and also with omp
+
+# 2. Add code here to run the program with mpirun -np $procs program_name
+
+# 3. Then submit this script to the compute nodes. 
+#    Instructions for how to do this are in the lab
